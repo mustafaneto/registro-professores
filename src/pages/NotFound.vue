ee014b1e7ebd3da274edfc55623bb73f17ed68d7
mustafaneto <mustafaneto26@gmail.com>
2022-02-16 15:12:43 -0500

    projeto-professor

---
+++ src/pages/NotFound.vue
@@ -0,0 +1,11 @@
+<template>
+  <section>
+    <base-card>
+      <h2>Página não encontrada</h2>
+      <p>
+        Está página não foi encontrada - por favor, clique
+        <router-link to="/coaches">aqui</router-link> para ver os professores.
+      </p></base-card
+    >
+  </section>
+</template>
